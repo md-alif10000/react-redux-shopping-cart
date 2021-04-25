bd24df92d138d0b31ff63856343a9f31bcf2dbf6
md_alif <md.alif10000@gmail.com>
2021-04-26 01:01:36 +0600

    created-product-component

---
+++ src/components/Products.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import  formatCurrency  from "../util.js";
+
+export default class Products extends Component {
+    render() {
+        return (
+					<div>
+						{!this.props.products ? (
+							<div>Loading</div>
+						) : (
+							<ul className='products'>
+								{this.props.products.map((product) => (
+									<li key={product._id}>
+										<div className='product'>
+											<a href={"#" + product._id}>
+												<img src={product.image} alt={product.title} />
+												<p>{product.title}</p>
+											</a>
+											<div className='product-price'>
+												<div>{formatCurrency(product.price)}</div>
+												<button className='button primary'>Add TO Cart</button>
+											</div>
+										</div>
+									</li>
+								))}
+							</ul>
+						)}
+					</div>
+				);
+    }
+}
